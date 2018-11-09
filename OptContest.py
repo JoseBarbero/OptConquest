5a6a110bc07e1a7f1fdeced0a1c682fbd99fdfca
José A. Barbero <jo94se@hotmail.com>
2018-11-09 11:52:59 +0100

    Added some diversifying methods

--- OptContest.py
+++ OptContest.py
@@ -4,10 +4,15 @@ from RandomAlgorithm import *
 
 # ini = time.time()
 # print(evaluate_algorithm(evolutive_algorithm, [read_file("Datasets/Doc11.txt"), 100]))
-# print(evolutive_algorithm(read_file("Datasets/Doc11.txt"), 50, elite_size=1, mut_ratio=10))
+print(evolutive_algorithm(read_file("Datasets/Doc11.txt"), pop_size=150,
+                          time_=60, elite_size=5, mut_ratio=0.05, diversify_size=0, not_improving_limit=3,
+                          sel_f=median_selection,
+                          elite_f=get_elite,
+                          rep_f=ox_reproduction,
+                          mut_f=mutate))
 # fin = time.time()
 # print("Time spend:", fin-ini)
 
-print(find_best_params(read_file("Datasets/Doc11.txt")))
+# print(find_best_params(read_file("Datasets/Doc11.txt")))
 
 # print(random_algorithm(read_file("Datasets/Doc11.txt"), 10000))
